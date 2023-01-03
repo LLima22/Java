afdad16374c7d4f790f54c2244aeed409dadce40
Lucas Limas <lucaslimagomes09@gmail.com>
2023-01-02 21:18:15 -0300

    first commit

---
+++ src/menu/Main.java
@@ -0,0 +1,12 @@
+package menu;
+
+
+public class Main {
+
+     public static void main(String[] args) {
+
+          Menu m = new Menu();
+          
+          m.menu();
+}
+}
---
+++ src/menu/Menu.java
@@ -0,0 +1,57 @@
+
+package menu;
+import static java.lang.System.exit;
+import java.util.LinkedList;
+import java.util.Queue;
+import javax.swing.JOptionPane;
+
+
+public class Menu {
+    Queue <String> Fila = new LinkedList<>();
+    
+    public int menu(){
+         
+        
+    int escolha = (int) Float.parseFloat(JOptionPane.showInputDialog("1 – Inserir Pacientes \n"+
+            "2 – Chamar o paciente para ser atendido \n"+
+            "3 – Visualizar Fila (cheia ou vazia)? \n"+
+            "4 – Próximo Paciente a ser atendido \n"+
+            "5 – Quantidade de Pacientes na fila \n"+
+            "6 – Sair"));
+
+    switch(escolha){
+               case 1:
+                     String nome = JOptionPane.showInputDialog("Insira o nome do paciente");
+                     Fila.add(nome);
+
+                   break;
+                   
+               case 2:
+                    String nomeBuscado = JOptionPane.showInputDialog("Pesquise o nome do paciente");
+                  Fila.contains(nomeBuscado);
+                  String mensagem2 = "chamando o paciente "+nomeBuscado;
+            JOptionPane.showMessageDialog(null, mensagem2);
+                   break;
+                   
+               case 3:
+            String mens = ""+Fila;
+            JOptionPane.showMessageDialog(null, mens);
+                   break;
+               case 4:
+                    String mensagem = "O paciente "+Fila.poll()+" será atendido agora";
+            JOptionPane.showMessageDialog(null, mensagem);
+                    break;
+               case 5:
+                   int numeroDePacientes;
+                   String mensagem3 = "O número de pacientes na fila é: "+Fila.size();
+                   JOptionPane.showMessageDialog(null, mensagem3);
+                   break;
+
+                case 6:
+                exit(0);
+    }
+    return menu();
+}
+}
+    
+
